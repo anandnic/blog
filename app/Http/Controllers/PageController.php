fa21ea1a488ed9ab2e9179e161ff0ba1c762f92f
unknown <ananddey.nic@gmail.com>
2019-12-29 22:48:41 +0530

    intro to MVC in laravel

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Page;
+class PageController extends Controller
+{
+    //
+    public function index(){
+        
+        $fruits=Page::fruits();
+        return view('index',['fruits'=>$fruits]);
+    }
+
+}
