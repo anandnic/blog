fd1478f6bda044214821db71b38b4ca0a2877f22
unknown <ananddey.nic@gmail.com>
2020-01-03 23:46:35 +0530

    mater page added

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+@section('content')
+<div class="card">
+<div class="card-header">
+<h2>{{$post->title}}</h2>
+</div>
+<div class="card-body">
+{{$post->content}}
+</div>
+</div>
+@endsection
